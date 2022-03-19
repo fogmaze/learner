92ad3d473b4a1ff04d27182249ef8f4da06cd98e
fogmaze <andy160830@gmail.com>
2022-03-19 15:59:02 +0800

    dev

--- idomLearner/findAns.py
+++ idomLearner/findAns.py
@@ -122,7 +122,7 @@ def getDefinition_idi(val:str)->str:
         return 'none'
     res = requests.get('https://dict.idioms.moe.edu.tw/'+params[0])
     soup = BeautifulSoup(res.text,'html.parser')
-    return '<'+soup.head.title.getText()[0:soup.head.title.getText().find(' [')]+'>'+ delEnter(readDefinitionFromForm_idi(soup))
+    return delEnter(readDefinitionFromForm_idi(soup))
 
 if __name__ == '__main__':
     print(getDefinition_both('不見經傳'))
